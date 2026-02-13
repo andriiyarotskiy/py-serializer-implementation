da8de3212f80f9e6c837c7b165b2fa93404ce2b6
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-02-13 22:56:31 +0000

    Serializer Implementation

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,38 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField()
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get(
+            "manufacturer", instance.manufacturer
+        )
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get(
+            "horse_powers", instance.horse_powers
+        )
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
+        instance.save()
+        return instance
+
+    def validate_horse_powers(self, value):
+        """
+        Check that the horse powers in allowed range
+        """
+        if value <= 0 or value >= 2000:
+            raise serializers.ValidationError()
+        return value
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.utils import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json_str = json.dumps(serializer.data, separators=(",", ":"))
+    return json_str.encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json: bytes) -> Car | None:
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return Car.objects.create(**serializer.validated_data)
+    return None
